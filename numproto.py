6548b3793c58959c068a60547d6cdc67fc565add
Rodolphe Marques <rodolphe.marques@xain.io>
2019-09-02 17:41:36 +0200

    Initial proposal

---
+++ numproto.py
@@ -0,0 +1,19 @@
+from io import BytesIO
+
+import numpy as np
+
+from ndarray_pb2 import NdArray
+
+
+def ndarray_to_proto(nda: np.ndarray) -> NdArray:
+    nda_bytes = BytesIO()
+    np.save(nda_bytes, nda)
+
+    return NdArray(ndarray=nda_bytes.getvalue())
+
+
+
+def proto_to_ndarray(nda_proto: NdArray) -> np.ndarray:
+    nda_bytes = BytesIO(nda_proto.ndarray)
+
+    return np.load(nda_bytes, allow_pickle=False)
