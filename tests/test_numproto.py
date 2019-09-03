6ae9c4894d7b38e72fe801db59b14806468a5e8c
Rodolphe Marques <rodolphe.marques@xain.io>
2019-09-03 15:10:57 +0200

    Parametrize tests

--- tests/test_numproto.py
+++ tests/test_numproto.py
@@ -1,28 +1,19 @@
+import pytest
 import numpy as np
 
 from numproto import ndarray_to_proto, proto_to_ndarray
 
-def test_numproto():
-    nda = np.arange(10)
-    result = proto_to_ndarray(ndarray_to_proto(nda))
-    assert np.array_equal(nda, result)
 
-    nda = np.arange(15).reshape(3, 5)
-    result = proto_to_ndarray(ndarray_to_proto(nda))
-    assert np.array_equal(nda, result)
-
-    nda = np.array([1.2, 3.5, 5.1])
-    result = proto_to_ndarray(ndarray_to_proto(nda))
-    assert np.array_equal(nda, result)
-
-    nda = np.array([[1, 2], [3, 4]], dtype=complex)
-    result = proto_to_ndarray(ndarray_to_proto(nda))
-    assert np.array_equal(nda, result)
-
-    nda = np.zeros((3, 4))
-    result = proto_to_ndarray(ndarray_to_proto(nda))
-    assert np.array_equal(nda, result)
-
-    nda = np.ones((2, 3, 4), dtype=np.int16)
+@pytest.mark.parametrize(
+    "nda",
+    [
+        np.arange(10),
+        np.arange(15).reshape(3, 5),
+        np.array([1.2, 3.5, 5.1]),
+        np.array([[1, 2], [3, 4]], dtype=complex),
+        np.zeros((3, 4)),
+        np.ones((2, 3, 4), dtype=np.int16),
+])
+def test_numproto(nda):
     result = proto_to_ndarray(ndarray_to_proto(nda))
     assert np.array_equal(nda, result)
