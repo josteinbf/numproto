e3047fa02aa8e37abbdde9e3c3dc2e580e065980
Rodolphe Marques <rodolphe.marques@xain.io>
2019-09-03 17:12:33 +0200

    Update black invocation to exclude auto generated proto files

--- numproto/ndarray_pb2.py
+++ numproto/ndarray_pb2.py
@@ -3,79 +3,67 @@
 # source: ndarray.proto
 
 import sys
-
-_b = sys.version_info[0] < 3 and (lambda x: x) or (lambda x: x.encode("latin1"))
+_b=sys.version_info[0]<3 and (lambda x:x) or (lambda x:x.encode('latin1'))
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
 from google.protobuf import reflection as _reflection
 from google.protobuf import symbol_database as _symbol_database
-
 # @@protoc_insertion_point(imports)
 
 _sym_db = _symbol_database.Default()
 
 
+
+
 DESCRIPTOR = _descriptor.FileDescriptor(
-    name="ndarray.proto",
-    package="",
-    syntax="proto3",
-    serialized_options=None,
-    serialized_pb=_b(
-        '\n\rndarray.proto"\x1a\n\x07NdArray\x12\x0f\n\x07ndarray\x18\x01 \x01(\x0c\x62\x06proto3'
-    ),
+  name='ndarray.proto',
+  package='',
+  syntax='proto3',
+  serialized_options=None,
+  serialized_pb=_b('\n\rndarray.proto\"\x1a\n\x07NdArray\x12\x0f\n\x07ndarray\x18\x01 \x01(\x0c\x62\x06proto3')
 )
+
+
 
 
 _NDARRAY = _descriptor.Descriptor(
-    name="NdArray",
-    full_name="NdArray",
-    filename=None,
-    file=DESCRIPTOR,
-    containing_type=None,
-    fields=[
-        _descriptor.FieldDescriptor(
-            name="ndarray",
-            full_name="NdArray.ndarray",
-            index=0,
-            number=1,
-            type=12,
-            cpp_type=9,
-            label=1,
-            has_default_value=False,
-            default_value=_b(""),
-            message_type=None,
-            enum_type=None,
-            containing_type=None,
-            is_extension=False,
-            extension_scope=None,
-            serialized_options=None,
-            file=DESCRIPTOR,
-        )
-    ],
-    extensions=[],
-    nested_types=[],
-    enum_types=[],
-    serialized_options=None,
-    is_extendable=False,
-    syntax="proto3",
-    extension_ranges=[],
-    oneofs=[],
-    serialized_start=17,
-    serialized_end=43,
+  name='NdArray',
+  full_name='NdArray',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='ndarray', full_name='NdArray.ndarray', index=0,
+      number=1, type=12, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b(""),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=17,
+  serialized_end=43,
 )
 
-DESCRIPTOR.message_types_by_name["NdArray"] = _NDARRAY
+DESCRIPTOR.message_types_by_name['NdArray'] = _NDARRAY
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
-NdArray = _reflection.GeneratedProtocolMessageType(
-    "NdArray",
-    (_message.Message,),
-    {
-        "DESCRIPTOR": _NDARRAY,
-        "__module__": "ndarray_pb2"
-        # @@protoc_insertion_point(class_scope:NdArray)
-    },
-)
+NdArray = _reflection.GeneratedProtocolMessageType('NdArray', (_message.Message,), {
+  'DESCRIPTOR' : _NDARRAY,
+  '__module__' : 'ndarray_pb2'
+  # @@protoc_insertion_point(class_scope:NdArray)
+  })
 _sym_db.RegisterMessage(NdArray)
 
 
